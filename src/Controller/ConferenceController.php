031ea049c6e2e55ddda970ae16558a9a88c4c8e3
Xin <43270698+sznzn@users.noreply.github.com>
2024-01-31 16:27:16 +0100

    Construire l'interface

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,27 +2,41 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-/////Ajoute quelques choses pour twig
-
+/////Ajoute quelques choses pour twig////
+use App\Repository\ConferenceRepository;
+use Twig\Environment;
+###################################
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
 
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-        return new Response(<<<EOF
-            <html>
-                <body>
-                    <img src="/images/under-construction.gif" alt="under-construction">
-                </body>
-            </html>
+        return $this->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]);
+    }
 
-        EOF
-    );
+    ###############################
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator= $commentRepository->getCommentPaginator($conference, $offset);
+
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+        ]);
     }
 }
